6925e8018cd59c6219582a811cad7011f24c6568
AshishPython11 <139869353+AshishPython11@users.noreply.github.com>
2025-02-12 09:44:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from routes import router
-
+import uvicorn
 app = FastAPI()
 
 #call router api from routes
@@ -11,6 +11,5 @@ def root():
     return {"message": "Welcome to the Blog API"}
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
 
